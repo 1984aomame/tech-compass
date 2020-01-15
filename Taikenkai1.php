8011ba771d57397f8e03bd962ed04a071a7a4332
野原僚香 <kafka.on.the.shoree@gmail.com>
2020-01-15 19:29:30 +0900

    体験学習の課題

---
+++ Taikenkai1.php
@@ -0,0 +1,23 @@
+<?php
+$input=5;
+$answer=array("D","C","B","A","S");
+
+
+if($input==1){
+    echo"成績は".$answer[0]."です";
+}elseif($input==2){
+    echo"成績は".$answer[1]."です";   
+
+}elseif($input==3){
+    echo"成績は".$answer[2]."です";    
+}elseif($input==4){
+    echo"成績は".$answer[3]."です";    
+}elseif($input==5){
+    echo"成績は".$answer[4]."です";
+    
+}
+
+
+
+
+?>
